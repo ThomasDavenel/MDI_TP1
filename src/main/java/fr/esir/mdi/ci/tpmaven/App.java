048f6a94570ef6d392c4e2b0afe4e02c8acba78c
ThomasDavenel <thomas.davenel@outlook.com>
2021-02-08 10:27:11 +0100

    changement branche 1

--- src/main/java/fr/esir/mdi/ci/tpmaven/App.java
+++ src/main/java/fr/esir/mdi/ci/tpmaven/App.java
@@ -5,6 +5,12 @@ package fr.esir.mdi.ci.tpmaven;
  */
 public class App {
   public static void main(String[]args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello World! -> modification branch 1");
   }
+  /**
+   * Branch_1
+   */
+  public static void branch_1(String[]args) {
+	    System.out.println("branche 1");
+	  }
 }
